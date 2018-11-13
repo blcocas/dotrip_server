b7d19525b3d4cba8fce0de7403bdb17f7c3c78be
blcocas <l02220hj@gmail.com>
2018-11-13 16:19:28 +0900

    schema

---
+++ schema_example.js
@@ -0,0 +1,26 @@
+user collection = {
+  "name"   : "이홍재",
+  "id"     : "hongss94",
+  "pass"   : 1234,
+  "dot"    : [
+  1234,3453,1534  //dot의 _id
+  ]
+}
+
+dot Collection = {
+  "_id"       : 1234,
+  "mainCity"  : "seoul",
+  "inDay"     : "181113",
+  "outDay"    : "191120",
+  "checkList" : [
+    { "id"       : '',
+      "title"    : '',
+      "actions"  : '',
+    },
+    {
+      "id"       : '',
+      "title"    : '',
+      "actions"  : '',
+    }
+  ]
+}
